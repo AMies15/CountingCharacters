911f58fd366280abc5207d48c790463bd1bce298
Alvin <Alvin>
2017-05-21 20:43:54 -0400

    countingCharacters2 Method added

--- CountingCharacters.js
+++ CountingCharacters.js
@@ -5,8 +5,22 @@ function countingCharacters(stringToCount){
               stringToCount.length + " characters.");
 }
 
+function countingCharacters2(stringToCount, characterToFind){
+    var characterCount = 0;
+    for (var characterPosition = 0; characterPosition < stringToCount.length; characterPosition++){
+        if (stringToCount[characterPosition] == characterToFind){
+            characterCount++;
+        }
+    }
+    console.log("String to search in: " + stringToCount);
+    console.log("Character to find: " + characterToFind);
+    console.log("Number of times the character appears: " + characterCount);
+}
+
 countingCharacters("cake");
 
 var userInput = prompt("Type in a word.");
 
 countingCharacters(userInput);
+
+countingCharacters2("xxaxxxbxx","x");
